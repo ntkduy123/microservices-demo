d9b6b7a66d6fba3bc29151d93b3370bddcfa5fc2
duy.nguyenthuykhan <duy.nguyenthuykhan@lazada.com>
2021-09-14 20:11:46 +0700

    init

---
+++ config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
@@ -0,0 +1,9 @@
+package com.microservices.demo.config.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+}
